4049a29fe2ce3e8f77de680db7781bd844ef670f
JOSE DANIEL OLIVEIRA SARAIVA <jd.saraiva@gmail.com>
2017-10-23 14:18:54 -0200

    Acertos

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/*', function(req, res) {
     res.sendFile(path.resolve('public/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
 });
